c13dcecefe44faba4374ffbfd30120b271c6badd
Doston <dtelectronics2108@gmail.com>
2026-01-08 23:17:06 +0500

    Implement custom user model and registration functionality

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Qo'shimcha Ma'lumotlar", {"fields": ("age", "phone")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("age", "phone")}),
+    )
+    list_display = UserAdmin.list_display + ("age", "phone")
+    ordering = ("username",)
+    search_fields = UserAdmin.search_fields + ("phone",)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "User Profile"
+        verbose_name_plural = "User Profiles"
+
+    def __str__(self):
+        return self.username
