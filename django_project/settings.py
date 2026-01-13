1227fb0ff69fecf8eecc166ffd6b8e1fee38d7d1
Doston <dtelectronics2108@gmail.com>
2026-01-13 22:13:44 +0500

    Add soft delete functionality and update core app settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,6 +33,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Local apps
+    "core.apps.CoreConfig",
     "home.apps.HomeConfig",
     "accounts.apps.AccountsConfig",
 ]
